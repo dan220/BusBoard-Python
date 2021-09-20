0667b2262d3506ec678118aa7bcf58a2877f508a
dstorer <daniel.storer@altran.com>
2021-09-20 16:46:11 +0100

    Added bus_stops class

---
+++ bus_stops.py
@@ -0,0 +1,8 @@
+class bus_stops:
+    bus_stops = []
+
+    def __init__(self):
+        pass
+
+    def add_bus_stops(self):
+        pass
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
-import json
 from buses import Buses
 #0180BAC30592
+
+def get_latandlong(postcode):
+    pass
+
+
 
 def main():
     print("Welcome to BusBoard.")
     atocode = input("Please input the bus stop atocode: ")
-    buses = Buses(atocode)
-    buses.update_timetable()
-    buses_text= buses.get_buses()
-    print(buses_text)
+    time_table = requests.get(
+            'https://transportapi.com/v3/uk/bus/stop/' + atocode + '/live.json?app_id=d469280a&app_key=004f69bc55849dfc0066c1c160a04424&group=no')
+    time_table = time_table.json()
+    buses = Buses(time_table)
+    buses.get_buses2()
+
+    bus_stops = requests.get('https://transportapi.com/v3/uk/places.json?lat=51.534121&lon=-0.006944&type=bus_stop&app_id=d469280a&app_key=004f69bc55849dfc0066c1c160a04424')
+
 
 
 
