facceacb05335d606553a5a9c256b29661b25f85
dstorer <daniel.storer@altran.com>
2021-09-22 11:46:08 +0100

    webpage contains info for board 1 but it is not inputted

--- bus_baord_web.py
+++ bus_baord_web.py
@@ -5,3 +5,12 @@ class Bus_board_web(Bus_board):
     def __init__(self):
         super().__init__()
 
+    def return_board(self, atcocode):
+        board = []
+        self.buses.get_buses(atcocode)
+        bus_list = self.buses.get_bus_information()
+        print(f'Stop -> {self.buses.time_table["name"]}')
+        for i in bus_list:
+            board.append(({i[0]},{i[1]},{i[2]}))
+        return board
+
--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from bus_stops import Bus_stops
+from bus_baord_web import Bus_board_web
 
 app = Flask(__name__)
 
@@ -9,6 +11,12 @@ def index():
 @app.route("/busInfo")
 def busInfo():
     postcode = request.args.get('postcode')
-    return render_template('info.html', postcode=postcode)
+    bus_stops = Bus_stops()
+    bus_board = Bus_board_web()
+    bus_stops.update_bus_stops(postcode)
+    first_stop_atcocode, second_stop_atcocode = bus_stops.get_atcocodes()
+    board_1 = bus_board.return_board(first_stop_atcocode)
+    board_2 = bus_board.print_board(second_stop_atcocode)
+    return render_template('info.html', board=board_1)
 
 if __name__ == "__main__": app.run()
